70714326e17a7ba5e498cc75944917b1aa0c6607
SerenaMalusa <sere.malu@gmail.com>
2024-02-06 16:29:40 +0100

    feat: get one random email from api and print in page

--- js/script.js
+++ js/script.js
@@ -3,13 +3,23 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        email: '',
         }
     }, 
+    methods: {
+        getEmailAPI() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((reply) => {
+                    const result = reply.data.response;
+                    console.log(result);
+                    this.email = result;
+                });
+        }
+    },
+
     mounted() {
-        
-        console.log(this.message);
-        
+        // console.log(this.message);
+        this.getEmailAPI();
     }
 });
 
